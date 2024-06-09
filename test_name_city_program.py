4ac6c36028c24715c5ffa74d0eba5e6392ecfa6c
Aleksandr <AlexKWGit@gmail.com>
2024-06-09 18:36:40 +0300

    Update test_name_city_program.py

--- test_name_city_program.py
+++ test_name_city_program.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 import io
 import sys
+from Name_city import name_city_program
 
 def get_output(func, inputs):
     captured_output = io.StringIO()
@@ -10,14 +11,11 @@ def get_output(func, inputs):
     sys.stdout = sys.__stdout__
     return captured_output.getvalue()
 
-def name_city_program(name, city):
-    return f'You name is {name}. You city is {city}'
-
 class TestNameCityProgram(unittest.TestCase):
 
     @patch('builtins.input', side_effect=['John', 'New York'])
     def test_name_city_program(self, mock_input):
-        output = get_output(name_city_program, ['John', 'New York'])
+        output = get_output(name_city_program, [])
         self.assertEqual(output.strip(), 'You name is John. You city is New York')
 
 if __name__ == '__main__':
